15fab1c9ae75a3908d69fef5e572e01f33bc713e
Bianco Carvalho <biancocarvalho@hotmail.com>
2017-06-01 14:36:17 -0300

    Inicial Commit

---
+++ Ladulceria.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ladulceria.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class Ladulceria
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Ladulceria.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ladulceria.LojaVirtual.Web.MvcApplication" Language="C#" %>
